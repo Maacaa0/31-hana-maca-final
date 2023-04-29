dece36789ccf00e0344eb9b64a1163fdb749a37e
Maacaa0 <hesss.marcel@gmail.com>
2023-04-30 00:17:07 +0200

    added hamburger + mobile menu

--- main.js
+++ main.js
@@ -7,16 +7,36 @@ const scrollContainer = () => {
   };
   
   document.addEventListener("scroll", () => {
-    // setTimeout(() => {
-    //   stickyMenu.classList.add("hidden");    keeps setting timeout 
-    // }, "5000");
-    // clearTimeout();
-    
-    if (scrollContainer().scrollTop > 900) {
-      scrollBtn.classList.remove("hidden");
+    // window.innerWidth > 1000 ?  ♦♦♦♦♦ ZMĚNĚNO ♦♦♦♦♦♦       dont show dynamic menu on smaller screen
+    if (scrollContainer().scrollTop > 900 && window.innerWidth > 1000) {
       dynamicMenu.classList.remove("hidden")
     } else {
-      scrollBtn.classList.add("hidden");
       dynamicMenu.classList.add("hidden")
     }
+
+    if (scrollContainer().scrollTop > 900) {
+      scrollBtn.classList.remove("hidden");
+    } else {
+      scrollBtn.classList.add("hidden");
+    }
   });
+
+  // ♦♦♦♦♦♦♦♦♦ ♦♦♦♦♦♦♦♦♦ ♦♦♦♦♦♦♦♦♦ ♦♦♦♦♦♦♦♦♦
+
+  // for showing mobile menu
+  const hamburgerBtn = document.getElementById("hamburgerBtn");
+  const menu = document.getElementById("menu");
+
+  hamburgerBtn.addEventListener("click", () => {
+    if (hamburgerBtn.classList.contains("active")) {
+      hamburgerBtn.classList.remove("active");
+      menu.classList.remove("shown");
+    }
+    else {
+      hamburgerBtn.classList.add("active");
+      menu.classList.add("shown");
+    }
+  });
+
+  // ♦♦♦♦♦♦♦♦♦ ♦♦♦♦♦♦♦♦♦ ♦♦♦♦♦♦♦♦♦ ♦♦♦♦♦♦♦♦♦
+
