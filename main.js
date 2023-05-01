8b97e935d6fb74a593a2ee43718a15b66047da4a
Maacaa0 <hesss.marcel@gmail.com>
2023-05-01 14:51:29 +0200

    handle click outside menu, about section bg test ?, responsive about section

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const scrollContainer = () => {
   };
   
   document.addEventListener("scroll", () => {
-    // window.innerWidth > 1000 ?  ♦♦♦♦♦ ZMĚNĚNO ♦♦♦♦♦♦       dont show dynamic menu on smaller screen
+    // dont show dynamic menu on smaller screen && open menu onscroll
     if (scrollContainer().scrollTop > 900 && window.innerWidth > 1000) {
       dynamicMenu.classList.remove("hidden")
     } else {
@@ -28,13 +28,27 @@ const scrollContainer = () => {
   const menu = document.getElementById("menu");
 
   hamburgerBtn.addEventListener("click", () => {
-    if (hamburgerBtn.classList.contains("active")) {
+    // if (hamburgerBtn.classList.contains("active")) {
+      hamburgerBtn.classList.toggle("active");
+      menu.classList.toggle("shown");
+    // }
+    // else {
+    //   hamburgerBtn.classList.add("active");
+    //   menu.classList.add("shown");
+    // }
+  });
+
+  // close menu onclick outside
+  document.addEventListener('click', event => {
+    const isClickInside = menu.contains(event.target);
+    const btnClick = hamburgerBtn.contains(event.target);
+    if (btnClick && hamburgerBtn.classList.contains("active")) {
+        hamburgerBtn.classList.add("active");
+        menu.classList.add("shown");
+
+    } else if (!isClickInside || event.target === hamburgerBtn) {
       hamburgerBtn.classList.remove("active");
       menu.classList.remove("shown");
-    }
-    else {
-      hamburgerBtn.classList.add("active");
-      menu.classList.add("shown");
     }
   });
 
