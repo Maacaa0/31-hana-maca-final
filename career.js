52256eee05ea808eea21bc686c99a6fd6969bad8
Maacaa0 <102512313+Maacaa0@users.noreply.github.com>
2023-05-10 22:12:27 +0200

    Add files via upload

---
+++ career.js
@@ -0,0 +1,115 @@
+const data = {
+  "positions": [
+    {
+      "name": "Kuchař",
+      "rank": "cetar",
+      "index": 0,
+      "description1": "She is the leader of the team.",
+      "description2": "She is brave and smart.",
+      "description3": "She likes to solve puzzles."
+    },
+    {
+      "name": "Spojař",
+      "rank": "desatnik",
+      "index": 1,
+      "description1": "He is the second in command.",
+      "description2": "He is strong and loyal.",
+      "description3": "He likes to shoot targets."
+    },
+    {
+      "name": "Právník",
+      "rank": "kapitan",
+      "index": 2,
+      "description1": "He is the newest member of the team.",
+      "description2": "He is fast and agile.",
+      "description3": "He likes to hack computers."
+    },
+      {
+      "name": "Provianťák",
+      "rank": "nadporucik",
+      "index": 3,
+      "description1": "She is the leader of the team.",
+      "description2": "She is brave and smart.",
+      "description3": "She likes to solve puzzles."
+    },
+    {
+      "name": "Řidič",
+      "rank": "rotny",
+      "index": 4,
+      "description1": "He is the second in command.",
+      "description2": "He is strong and loyal.",
+      "description3": "He likes to shoot targets."
+    },
+  ]
+};
+
+
+const BREAKPOINT_TABLET = 1000;
+const BREAKPOINT_MOBILE = 700;
+
+function handleScreenWidth() {
+  if (window.innerWidth < BREAKPOINT_MOBILE) {
+    swiperCareer.setAttribute("slides-per-view", "1");
+  } else if (window.innerWidth < BREAKPOINT_TABLET) {
+    swiperCareer.setAttribute("slides-per-view", "2");
+  } else {
+    swiperCareer.setAttribute("slides-per-view", "3");
+  }
+}
+
+window.addEventListener("resize", handleScreenWidth);
+
+
+const swiperCareer = document.getElementById("swiperCareer");
+
+let jobsContainer = document.querySelector(".mySwiper");   
+
+window.addEventListener("load", () => {
+
+   
+
+    data.positions.map((job, jobsIndex) => {
+
+      const swiperSlide = document.createElement("swiper-slide");
+      swiperSlide.className = "career_swiperItem job";
+
+      const swiperItemHeader = document.createElement("div");
+      swiperItemHeader.className = "swiperItem_header";
+
+      const swiperItemPosition = document.createElement("p");
+      swiperItemPosition.className = "swiperItem_position";
+      swiperItemPosition.textContent = data.positions[jobsIndex].name;
+
+      const swiperItemRank = document.createElement("img");
+      swiperItemRank.className = "swiperItem_rank";
+      swiperItemRank.src = `images/hodnosti/${data.positions[jobsIndex].rank}.svg`;
+
+
+      const positionDescription = document.createElement("p");
+      positionDescription.className = "swiperItem_description";
+      positionDescription.textContent = `${data.positions[jobsIndex].description1}
+                                         ${data.positions[jobsIndex].description2}
+                                         ${data.positions[jobsIndex].description3}`
+
+      swiperItemHeader.appendChild(swiperItemPosition);
+      swiperItemHeader.appendChild(swiperItemRank);
+      swiperSlide.appendChild(swiperItemHeader);
+      swiperSlide.appendChild(positionDescription);
+
+      jobsContainer.appendChild(swiperSlide);
+
+    // let position = job.querySelector(".swiperItem_position");
+    // let rank = job.querySelector(".swiperItem_rank");
+    // let description1 = job.querySelector(".swiperItem_description1");
+    // let description2 = job.querySelector(".swiperItem_description2");
+    // let description3 = job.querySelector(".swiperItem_description3");
+
+    // position.textContent = data.positions[jobsIndex].name;
+    // rank.src = `images/hodnosti/${data.positions[jobsIndex].rank}.svg`;
+    // description1.textContent = data.positions[jobsIndex].description1;
+    // description2.textContent = data.positions[jobsIndex].description2;
+    // description3.textContent = data.positions[jobsIndex].description3;
+    });
+});
+
+
--- main.js
+++ main.js
@@ -28,14 +28,8 @@ const scrollContainer = () => {
   const menu = document.getElementById("menu");
 
   hamburgerBtn.addEventListener("click", () => {
-    // if (hamburgerBtn.classList.contains("active")) {
       hamburgerBtn.classList.toggle("active");
       menu.classList.toggle("shown");
-    // }
-    // else {
-    //   hamburgerBtn.classList.add("active");
-    //   menu.classList.add("shown");
-    // }
   });
 
   // close menu onclick outside
@@ -52,7 +46,7 @@ const scrollContainer = () => {
     }
   });
 
-
+// swipe to close menu 
 let touchStart, touchEnd;
 
 menu.addEventListener(
@@ -66,10 +60,101 @@ menu.addEventListener(
 );
 
 menu.addEventListener('touchend', e => {
-  if (touchStart - touchEnd > 45) {
-    menu.classList.remove('shown')
+  if (touchEnd - touchStart > 45) {
+    hamburgerBtn.classList.remove("active");
+    menu.classList.remove('shown');
   }
 })
 
   // ♦♦♦♦♦♦♦♦♦ ♦♦♦♦♦♦♦♦♦ ♦♦♦♦♦♦♦♦♦ ♦♦♦♦♦♦♦♦♦
 
+ 
+
+  const historyData = {
+    chemVojsko: {
+        title: "Chemické vojsko",
+        content: `
+        <article class="history_content_data">
+        <h4>1919-1945</h4>
+        
+        <p>Zkušenosti s bojovými chemickými látkami během 1. světové války vedly k vytvoření československé armády a urychlenému řešení potřebných ochranných prostředků. V roce 1919 byla založena zkušební a opravárenská jednotka, která se zabývala vývojem a testováním nových ochranných prostředků. Ústav pro plynovou službu v Olomouci, založený v roce 1921, se stal základem výstavby československého chemického vojska. V počátcích se armáda opírala o ochranné masky z výzbroje armád bojujících na frontách 1. světové války, včetně francouzské masky ARS a německých kožených masek. V roce 1923 byla do výzbroje zavedena první československá vojenská ochranná maska, a to maska vz. 23, vyvinutá firmou Horák Praha. V roce 1935 se stal první chemickou jednotkou 401. dělostřelecký oddíl v Olomouci, a v první polovině 30. let byly vyvinuty dvě nové prototypy vojenských ochranných masek, maska vz. 33 a maska vz. 35, která se stala standardní armádní maskou s výrobou téměř 1,5 milionu kusů.</p>
+        </article>
+        
+        <article class="history_content_data">
+        <h4>1945-1989</h4>
+        
+        <p>V květnu 1945 byla vytvořena skupina zvláštních bojových prostředků a obnoven byl Vojenský chemický ústav. Pplk. gšt. Oskar Schreiber navrhl v roce 1945 zřízení chemického vojska, ale tento nápad byl realizován až v roce 1949. Hlavními součástmi chemického vojska byly chemický prapor 101 v Jaroměři a 103 v Šafárikově. V roce 1952 byl zřízen 105. plamenometný prapor v Liberci. Po druhé světové válce byly v československé armádě nejčastěji používané trofejní německé ochranné masky vz. 30N a vz. 38N, ale později byly vyvinuty domácí masky, jako například maska ŠM-41 a M52. Od 2. poloviny 50. let byla koncepce činnosti chemického vojska zaměřena na předpoklad masového použití zbraní hromadného ničení. V roce 1968 byl v Liberci ustaven 105. pluk chemické ochrany a v roce 1972 byla zahájena výroba nové masky M10.</p>
+        </article>`,
+        imgs: ["lbc_vojsko.png", "lbc_vojsko1.png", "lbc_vojsko2.png"]
+    },
+
+    lbcVojsko: {
+        title: "Vojsko v Liberci",
+        content: `<article class="history_content_data">
+        <h4>VZNIK ČESKOSLOVENSKÉHO CHEMICKÉHO VOJSKA</h4>
+        
+        <p>V květnu 1945 byla založena Skupina zvláštních bojových prostředků a obnoven Vojenský chemický ústav. V srpnu 1945 navrhl velitel Skupiny zvláštních bojových prostředků, Oskar Schreiber, zřízení samostatného chemického vojska. Nicméně bylo to až v roce 1949, kdy prezident republiky podepsal dekret, kterým bylo založeno chemické vojsko. Toto vojsko se skládalo z chemických praporů 101 a 103. V roce 1952 byl zřízen 105. plamenometný prapor a věnována pozornost radiologické ochraně. V roce 1957 byla založena 1. brigáda chemické ochrany a v roce 1977 102. brigáda chemické ochrany. V Liberci byl dislokován 51. prapor chemické ochrany, 61. prapor odmořování v terénu a 103. prapor chemické ochrany.</p>
+        </article>
+
+        <article class="history_content_data">
+        <h4>CHEMICKÉ VOJSKO PO ROCE 1989</h4>
+        
+        <p>Po roce 1989 pokračovalo chemické vojsko v činnosti, včetně účasti v Perském zálivu v letech 1990-1991. Po rozdělení Československa v roce 1993 se stala Výcviková a mobilizační základna chemického vojska v Liberci reprezentantem chemického vojska v ČR. Po úspěšné účasti v mezinárodních operacích se vstupem ČR do NATO se stala součástí sil okamžité reakce NATO a 9. roty chemické ochrany. V roce 2005 vznikla 31. brigáda radiační, chemické a biologické ochrany, která se 1. prosince 2013 zreorganizovala na 31. pluk radiační, chemické a biologické ochrany. Jedná se o jediný specializovaný svazek Armády České republiky určený pro řešení úkolů chemického zabezpečení a odstraňování následků po použití zbraní hromadného ničení. Chemické jednotky se účastnily operací v Saúdské Arábii, Kuvajtu, Iráku, Afghánistánu, Bosně a Hercegovině, Kosovu, a při ochraně olympijských her v Řecku a Summitu NATO v Turecku. Současné chemické vojsko navazuje na tradice armádních chemiků v meziválečném období a příslušníci se stále nasazují v mírových misích a proti mezinárodnímu terorismu.</p>
+        </article>`,
+        imgs: ["lbc_vojsko3.png", "lbc_vojsko4.png", "lbc_vojsko5.png"]
+    },
+
+    kasarna: {
+        title: "Kasárna",
+        content: `<article class="history_content_data">
+        <h4>DOLNÍ KASÁRNA</h4>
+        
+        <p>Kasárna císaře Františka Josefa v Liberci byla postavena v letech 1891-1893 a sloužila jako sídlo 94. pěšího pluku rakousko-uherské armády. V době krize byly do Liberce přemístěny další jednotky. Po okupaci Německem byla kasárna obsazena německými vojáky. Po druhé světové válce byl pěší pluk přejmenován na 30. pěší pluk a kasárna získala název Kasárna 6. října. Kasárenský komplex se skládá z osmi budov a čtyř menších, přičemž dominantou je novorománská budova štábu zdůrazněná čtyřbokou věží. V roce 1944 se československé jednotky poprvé dostaly na československé území při Karpatsko-Dukelské operaci, která trvala od 8. září do 1. listopadu 1944.</p>
+        </article>
+
+        <article class="history_content_data">
+        <h4>HORNÍ KASÁRNA</h4>
+        
+        <p>Kasárna "Erzherzog Franz Ferdinand a Infanterie Kaserne" byla postavena v letech 1911-1912 pro oddíly dělostřelectva rakousko-uherské armády. Kasárna se skládá ze 8 budov v pozdně secesní architektuře, s různými vnitřními prostory, jako stáje, věznice a světnice. Název "Kasárna Jana Žižky" byl přidělen až po druhé světové válce. Jan Žižka byl významný husitský vojevůdce, považovaný za otce husitské vojenské doktríny a autora defenzivní bojové techniky. Účastnil se mnoha bitvách a kampaní, včetně tažení proti řádu německých rytířů a bojů s katolickou šlechtou. Jan Žižka se účastnil první české defenestrace a stal se hejtmanem Tábora. Poranění obou očí ho nakonec zcela oslepila, ale i tak pokračoval v boji. Zemřel v roce 1424 na hlíznaté onemocnění při obléhání hradu v Přibyslavi.</p>
+        </article>`,
+        imgs: ["lbc_vojsko6.png", "lbc_vojsko7.png", "lbc_vojsko8.png"]
+    }
+}
+
+const historyLink = document.querySelectorAll(".history_link");
+const historyPrevBtn = document.getElementById("historyPrevBtn");
+const historyNextBtn = document.getElementById("historyNextBtn");
+const historyCloseBtn = document.getElementById("historyCloseBtn");
+let historyContent = document.getElementById("history_content");
+let historyTextContent = document.getElementById("historyText");
+let historyImgs = document.querySelectorAll(".history_content_img");
+let historyImgsArray = Array.from(historyImgs);
+
+  historyLink.forEach(link => {
+    link.addEventListener("click", function() {
+      historyText.innerHTML = historyData[link.id].content;
+      historyTitle.textContent = historyData[link.id].title;
+      historyImgsArray.map((img, index) => img.src = `images/${historyData[link.id].imgs[index]}`)
+      historyContent.classList.remove("hidden");
+    })
+  })
+
+  historyCloseBtn.addEventListener("click", () => {
+      historyContent.classList.add("hidden");
+  })
+
+
+//   let historyKeys = Object.keys(historyData);
+  
+//   historyPrevBtn.addEventListener("click", () => {
+//     let currentPage = historyKeys.indexOf(historyTitle.textContent);
+//     console.log(currentPage);
+//     // historyText.innerHTML = historyData[link.id].content;
+//     //   historyTitle.textContent = historyData[link.id].title;
+//     //   historyImgsArray.map((img, index) => img.src = `images/${historyData[link.id].imgs[index]}`)
+//  })
+
+//  historyNextBtn.addEventListener("click", () => {
+//     let currentPage = historyKeys.indexOf(historyTitle.textContent);
+//     console.log(historyTitle.textContent)
+// })
